71c60ac405eef9205b1aa565d01a3b7dbfa6c2e8
Alok <alokme123@gmail.com>
2021-03-19 16:31:52 +0530

    first commit

--- firstapp/ViewController.swift
+++ firstapp/ViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var helloText: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Do any additional setup after lsoading the view.
+        
     }
-
-
+    
+    @IBAction func doneNewButton(_ sender: Any) {
+        helloText.text = "Hello, "+(textField.text)!
+    }
 }
 
