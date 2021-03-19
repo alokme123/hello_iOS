a429c3b640ca9d28b11c7552b09490cce789a1ab
Alok <alokme123@gmail.com>
2021-03-19 11:16:42 +0530

    Initial Commit

---
+++ firstapp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  firstapp
+//
+//  Created by Alok N on 19/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
